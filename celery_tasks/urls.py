34ec68650b8a3be2c8c1df11dfb199e63210d16d
Your Name <you@example.com>
2021-12-16 16:47:26 +0600

    add url testmodels

--- celery_tasks/urls.py
+++ celery_tasks/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from .views import test
+from .views import test, TestListView
 
 
 urlpatterns = [
-    path('test/',test,name='test')
+    path('test/',test,name='test'),
+    path('models/',TestListView.as_view(),name='test_models_url')
 ]
